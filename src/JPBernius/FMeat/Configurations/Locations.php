346425098c0ef4d3f7d7a884788e3be8cf213a6d
Jan Philip Bernius <janphilip.bernius@tum.de>
2017-12-05 15:24:52 +0100

    Add more Locations

--- src/JPBernius/FMeat/Configurations/Locations.php
+++ src/JPBernius/FMeat/Configurations/Locations.php
@@ -10,20 +10,125 @@ namespace JPBernius\FMeat\Configurations;
 interface Locations
 {
     /**
+     * FMI Bistro Garching
      * Wilhelm Gastronomie - TUM Garching - Mathe/Informatik
+     * Boltzmannstraße 3, 85748 Garching
      * @url http://www.wilhelm-gastronomie.de/tum-fmi-bistro
      */
     const FMI_BISTRO = 'fmi-bistro';
 
     /**
+     * IPP Bistro Garching
      * Konradhof Catering - Max-Planck-Institut für Plasmaphysik
+     * Boltzmannstraße 2, 85748 Garching
      * @url https://konradhof-catering.de/ipp/
      */
     const IPP_BISTRO = 'ipp-bistro';
 
     /**
-     * Mensa Garching - Studentenwerk München
-     * @url http://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_422_-de.html
+     * Mensa Arcisstraße
+     * Arcisstraße 17, München
+     */
+    const MESA_ARCISSTRASSE = 'mensa-arcisstr';
+
+    /**
+     * Mensa Garching
+     * Lichtenbergstraße 2, Garching
      */
     const MENSA_GARCHING = 'mensa-garching';
+
+    /**
+     * Mensa Leopoldstraße
+     * Leopoldstraße 13a, München
+     */
+    const MENSA_LEOPOLDSTRASSE = 'mensa-leopoldstr';
+
+    /**
+     * Mensa Lothstraße
+     * Lothstraße 13d, München
+     */
+    const MENSA_LOTHSTRASSE = 'mensa-lothstr';
+
+    /**
+     * Mensa Martinsried
+     * Großhaderner Straße 6, Planegg-Martinsried
+     */
+    const MENSA_MARTINSRIED = 'mensa-martinsried';
+
+    /**
+     * Mensa Pasing
+     * Am Stadtpark 20, München
+     */
+    const MENSA_PASING = 'mensa-pasing';
+
+    /**
+     * Mensa Weihenstephan
+     * Maximus-von-Imhof-Forum 5, Freising
+     */
+    const MENSA_WEIHENSTEPHAN = 'mensa-weihenstephan';
+
+    /**
+     * StuBistro Arcisstraße
+     * Arcisstraße 12, München
+     */
+    const STUBISTRO_ARCISSTRASSE = 'stubistro-arcisstr';
+
+    /**
+     * StuBistro Goethestraße
+     * Goethestraße 70, München
+     */
+    const STUBISTRO_GOETHESTRASSE = 'stubistro-goethestr';
+
+    /**
+     * StuBistro Großhadern
+     * Butenandtstraße 13, Gebäude F, München
+     */
+    const STUBISTRO_GROSSHADERN = 'stubistro-grosshadern';
+
+    /**
+     * StuBistro Rosenheim
+     * Hochschulstraße 1, Rosenheim
+     */
+    const STUBISTRO_ROSENHEIM = 'stubistro-rosenheim';
+
+    /**
+     * StuBistro Schellingstraße
+     * Schellingstraße 3, München
+     */
+    const STUBISTRO_SCHELLINGSTRASSE = 'stubistro-schellingstr';
+
+    /**
+     * StuCafé Adalbertstraße
+     * Adalbertstraße 5, München
+     */
+    const STUCAFE_ADALBERTSTRASSE = 'stucafe-adalbertstr';
+
+    /**
+     * StuCafé Akademie Weihenstephan
+     * Alte Akademie 1, Freising
+     */
+    const STUCAFE_AKADEMIE_WEIHENSTEPHAN = 'stucafe-akademie-weihenstephan';
+    /**
+     * StuCafé Boltzmannstraße
+     * Boltzmannstraße 15, Garching
+     */
+    const STUCAFE_BOLTZMANNSTRASSE = 'stucafe-boltzmannstr';
+
+    /**
+     * StuCafé in der Mensa Garching
+     * Lichtenbergstraße 2, Garching
+     */
+    const STUCAFE_GARCHING = 'stucafe-garching';
+
+    /**
+     * StuCafé Karlstraße
+     * Karlstraße 6, München
+     */
+    const STUCAFE_KARLSTRASSE = 'stucafe-karlstr';
+
+    /**
+     * StuCafé Pasing
+     * Am Stadtpark 20, München
+     */
+    const STUCAFE_PASING = 'stucafe-pasing';
 }
