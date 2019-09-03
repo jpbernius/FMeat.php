df03c3aa1a73d0544f5115a639109c604fcacb6c
Jan Philip Bernius <janphilip.bernius@tum.de>
2019-09-03 20:17:21 +0200

    Update Urls.php (#100)

--- src/JPBernius/FMeat/Configurations/Urls.php
+++ src/JPBernius/FMeat/Configurations/Urls.php
@@ -10,8 +10,9 @@ namespace JPBernius\FMeat\Configurations;
 interface Urls
 {
     /**
-     * Eat-Api by Simon Rehwald
+     * Fork of eat-api by Simon Rehwald (@srehwald)
+     * @url https://github.com/jpbernius/eat-api
      * @url https://github.com/srehwald/eat-api
      */
-    const API_URL = 'https://srehwald.github.io/eat-api/%s/%s/%s.json';
+    const API_URL = 'https://jpbernius.github.io/eat-api/%s/%s/%s.json';
 }
